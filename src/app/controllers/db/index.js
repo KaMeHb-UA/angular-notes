d5b82f355ff31d812e060e66caac962c9514f799
KaMeHb-UA <marlock@etlgr.com>
2020-08-30 19:48:28 +0300

    fix async render changes detection with setImmediate, move storage to indexedDB

--- src/app/controllers/db/index.js
+++ src/app/controllers/db/index.js
@@ -1,10 +1,10 @@
 import { Observable } from 'rxjs'
-import * as provider from './providers/localStorage'
+import * as provider from './providers/indexedDB'
 
 export default function(table, name){
     return new Observable(observer => {
         provider.get(table, name).then(v => observer.next(v));
-        const handler = count => observer.next(count);
+        const handler = v => observer.next(v);
         provider.onChange(table, name, handler);
         return () => provider.offChange(table, name, handler)
     })
